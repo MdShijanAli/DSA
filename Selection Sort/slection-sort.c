4e3ac40439115f57b088a0830cb866eb7b5dc9bb
MdShijanAli <shijan135@gmail.com>
2025-11-24 09:22:04 +0600

    Update

--- Selection Sort/slection-sort.c
+++ Selection Sort/slection-sort.c
@@ -1,62 +1,40 @@
 #include<stdio.h>
 
-// void selectionSort(int arr[], int n) {
-//     for (int i = 0; i < n - 1; i++) {
-//         int minIndex = i;
-//         for (int j = i + 1; j < n; j++) {
-//             if (arr[j] < arr[minIndex]) {
-//                 minIndex = j;
-//             }
-//         }
-//         // Swap the found minimum element with the first element
-//         printf("Min Index: %d\n", minIndex);
-//         printf("Arr[minIndex]: %d\n", arr[minIndex]);
-//         int temp = arr[minIndex];
-//         printf("Arr[i]: %d\n", arr[i]);
-//         arr[minIndex] = arr[i];
-//         arr[i] = temp;
-//     }
-// }
-
-// void printArray(int arr[], int n) {
-//     for (int i = 0; i < n; i++) {
-//         printf("%d ", arr[i]);
-//     }
-//     printf("\n");
-// }
-
-void selectionSort(int arr[], int n){
-    for(int i = 0; i<n-1; i++){
+void selectionSort(int arr[], int size){
+    for(int i = 0; i< size-1; i++){
         int minIndex = i;
-        for(int j = i+1; j<n; j++){
-           if(arr[minIndex] > arr[j]){
-            minIndex = j;
-           }
+        for(int j = i+1; j<size; j++){
+            if(arr[minIndex]>arr[j]){
+                minIndex = j;
+            }
         }
-
         int temp = arr[minIndex];
         arr[minIndex] = arr[i];
-        arr[i] = temp;
+        arr[i]= temp;
     }
 }
 
-void printArray(int arr[], int n){
-    for(int i = 0; i<n; i++){
+void printArray(int arr[], int size){
+    for(int i = 0; i<size; i++){
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
-int main() {
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    
-    printf("Original array: \n");
-    printArray(arr, n);
-    
-    selectionSort(arr, n);
-    printf("Sorted array: \n");
-    printArray(arr, n);
+int main(){
+    int size;
+    printf("Enter size of array: ");
+    scanf("%d", &size);
 
-    return 0;
+    int arr[size];
+    printf("Enter %d elements:\n", size);
+    for(int i = 0; i<size; i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("Original array:\n");
+    printArray(arr, size);
+
+    selectionSort(arr, size);
+    printf("Sorted array:\n");
+    printArray(arr, size);
 }
