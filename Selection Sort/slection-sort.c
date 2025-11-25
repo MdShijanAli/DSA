cdad4227ddcd9b57f5c46ecc6eca8f6845665340
MdShijanAli <shijan135@gmail.com>
2025-11-25 23:32:21 +0600

    Refactor: Improve formatting and readability of sorting algorithms

--- Selection Sort/slection-sort.c
+++ Selection Sort/slection-sort.c
@@ -1,34 +1,42 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void selectionSort(int arr[], int size){
-    for(int i = 0; i< size-1; i++){
+void selectionSort(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
         int minIndex = i;
-        for(int j = i+1; j<size; j++){
-            if(arr[minIndex]>arr[j]){
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[minIndex] > arr[j])
+            {
                 minIndex = j;
             }
         }
         int temp = arr[minIndex];
         arr[minIndex] = arr[i];
-        arr[i]= temp;
+        arr[i] = temp;
     }
 }
 
-void printArray(int arr[], int size){
-    for(int i = 0; i<size; i++){
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
-int main(){
+int main()
+{
     int size;
     printf("Enter size of array: ");
     scanf("%d", &size);
 
     int arr[size];
     printf("Enter %d elements:\n", size);
-    for(int i = 0; i<size; i++){
+    for (int i = 0; i < size; i++)
+    {
         scanf("%d", &arr[i]);
     }
     printf("Original array:\n");
--- index.c
+++ index.c
@@ -1,10 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void selectionSort(int arr[], int  size){
-    for(int i = 0; i<size-1; i++){
+void selectionSort(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
         int minIndex = i;
-        for(int j = i+1; j<size; j++){
-            if(arr[minIndex] > arr[j]){
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[minIndex] > arr[j])
+            {
                 minIndex = j;
             }
         }
@@ -14,41 +18,50 @@ void selectionSort(int arr[], int  size){
     }
 }
 
-void insertionSortDescending(int arr[], int size){
-    for(int i = 1; i<size; i++){
+void insertionSortDescending(int arr[], int size)
+{
+    for (int i = 1; i < size; i++)
+    {
         int key = arr[i];
-        int j = i -1;
-        while(j>=0 && arr[j] < key){
-            arr[j+1] = arr[j];
+        int j = i - 1;
+        while (j >= 0 && arr[j] < key)
+        {
+            arr[j + 1] = arr[j];
             j--;
         }
-        arr[j+1] = key;
+        arr[j + 1] = key;
     }
 }
 
-void bubbleSortDescending(int arr[], int size){
-    for(int i = 0; i<size-1; i++){
-        for(int j = 0; j<size-i-1; j++){
-            if(arr[j] < arr[j+1]){
+void bubbleSortDescending(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] < arr[j + 1])
+            {
                 int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 }
 
-void printArray(int arr[], int size){
-    for(int i=0; i<size; i++){
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
-
-int main(){
-    int arr[] = {5,2,4,1,3};
-    int size = sizeof(arr)/sizeof(arr[0]);
+int main()
+{
+    int arr[] = {5, 2, 4, 1, 3};
+    int size = sizeof(arr) / sizeof(arr[0]);
 
     printf("Array elements: \n");
     printArray(arr, size);
